2f99767d4df129bd0a24221a53a0b3402e6fa350
colinbedlington-Kier <colin.bedlington@kier.co.uk>
2026-03-23 11:11:35 +0000

    Fix Assign Parameters execution path in external handler

--- src/DfEIfcNamer/ExternalEvents/RevitExecutionHandler.cs
+++ src/DfEIfcNamer/ExternalEvents/RevitExecutionHandler.cs
@@ -40,12 +40,7 @@ namespace DfEIfcNamer.ExternalEvents
                         response.SetupStatus = _cobieSyncService.CheckSetup(doc, _request.CategoryIds);
                         break;
                     case RevitRequestId.AssignParameters:
-                        using (var tx = new Transaction(doc, "DfE IFC Namer - Assign Parameters"))
-                        {
-                            tx.Start();
-                            response.SetupStatus = _cobieSyncService.AssignParameters(doc, _request.CategoryIds);
-                            tx.Commit();
-                        }
+                        response.SetupStatus = _cobieSyncService.AssignParameters(doc, _request.CategoryIds);
                         break;
                     case RevitRequestId.GetAvailableParameters:
                         response.InstanceParameters = _cobieSyncService.GetStringParameters(doc, false);
